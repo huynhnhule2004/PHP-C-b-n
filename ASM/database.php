bbafe14b2efee309ef3fdeaff279a73e060f9e46
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-03-15 19:16:56 +0700

    hiển thị sản phẩm

--- ASM/database.php
+++ ASM/database.php
@@ -11,7 +11,7 @@ $conn = mysqli_connect($server, $user, $pass, $db);
 if (!$conn) {
     die ("Connection failed: ". mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
  
 
 ?>
